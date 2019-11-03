07e0fe5d6b420ac18c99b60d72db2749ce9d0b0a
Rushikesh Magdum <Rushikesh.Magdum@asg.com>
2019-11-03 14:53:17 +0530

    launching chrome browser for testing through jenkins

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -4,11 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LoginTest {
+	
 	WebDriver driver = null;
 
 	@Test
 	public void login() {
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		System.out.println("login is executed");
