afe582c947ae8a68117e3d7970fcc08a9d605f02
Rushikesh Magdum <Rushikesh.Magdum@asg.com>
2019-11-03 14:45:30 +0530

    launching chrome browser

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,11 +1,16 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class LoginTest {
+	WebDriver driver = null;
 
 	@Test
 	public void login() {
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
 		System.out.println("login is executed");
 	}
 }
